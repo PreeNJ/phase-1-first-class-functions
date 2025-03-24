8d6d36efb8475fd6278c6c0dab1a784237608fc2
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-24 19:12:46 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-function receivesAFunction(callback) {
-    callback();
-}
+ function receivesAFunction(callback) {
+     callback();
+ }
 
-function returnsANamedFunction() {
-    function namedFunction() {
-        console.log("This is a named function.");
-    }
-    return namedFunction;}
+ function returnsANamedFunction() {
+   function namedFunction() {
+       console.log("This is a named function.");
+   }
+   return namedFunction;}
 
-    function returnsAnAnonymousFunction() {
-        return function() {
-            console.log("This is an anonymous function.");
-        };
-    }
+   function returnsAnAnonymousFunction() {
+       return function() {
+           console.log("This is an anonymous function.");
+       };
+   }
 
-receivesAFunction(() => console.log("Callback function executed."));
-const namedFunc = returnsANamedFunction();
-namedFunc(); 
-
+ receivesAFunction(() => console.log("Callback function executed."));
+ const namedFunc = returnsANamedFunction();
+ namedFunc(); 
 const anonymousFunc = returnsAnAnonymousFunction();
-anonymousFunc(); 
+anonymousFunc();
 
-const returnFirstTwoDrivers([`John Abadi`, `Jane Doe`, `Mary Smith`, `Bob Johnson`]) {
-    return [drivers[0], drivers[1]];
+const returnFirstTwoDrivers = function (drivers) {
+  //  array
+  return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+console.log(returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);
 };
